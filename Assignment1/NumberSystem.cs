b2f37fa318a3f3e5f4e6800092743d88fb3e99f8
Pankaj Goyal <pankaj.goyal@optym.com>
2020-05-30 11:01:35 +0530

    Added few more comments

--- Assignment1/NumberSystem.cs
+++ Assignment1/NumberSystem.cs
@@ -2,13 +2,16 @@
 {
     public class NumberSystem
     {
-        private INumberToWord NumberToWordConverter { get; set; }
+        // Converter can be either Indian or Internation type based on dependncy injection 
+        public INumberToWord NumberToWordConverter { get; private set; }
 
+        // Inject dependency via Constructor Injection
         public NumberSystem(INumberToWord numberToWordConverter)
         {
             this.NumberToWordConverter = numberToWordConverter;
         }
 
+        // Method for number to word conversion
         public string Convert(long number)
         {
             return this.NumberToWordConverter.ConvertNumberToWord(number);
