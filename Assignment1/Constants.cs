c7624534aa8049d92463bd7f0b5456f70ba2107d
Pankaj Goyal <pankaj.goyal@optym.com>
2020-05-30 10:30:38 +0530

    Removed unused namespaces

--- Assignment1/Constants.cs
+++ Assignment1/Constants.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Assignment1
+﻿namespace Assignment1
 {
     public static class Constants
     {
@@ -12,7 +8,7 @@ namespace Assignment1
         public static string[] digits = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
         public static string[] teens = { "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
         public static string[] tens = { "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-        public static string[] bigs = { "", "Thousand", "Million" };
+        public static string[] bigs = { "", "Thousand", "Million", "Billion" };
         public static string[] bigsIndian = { "Thousand", "Lakh", "Crore" };
         public static string hundred = "Hundred";
     }
